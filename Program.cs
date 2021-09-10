c9d0a27192f116fdf2d1133b278fe27b15f4776e
Макс <yashanty@gmail.com>
2021-09-10 15:52:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Anagram_Detection
+{
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            var a = "ab";
+            var b = "ba";
+            var c = a.ToCharArray();
+            if(String.Equals("ab", "ba"))
+                Console.WriteLine("true");
+            else Console.WriteLine("false");
+            Console.WriteLine(String.GetHashCode("ab"));
+            Console.WriteLine(String.GetHashCode("ba"));
+        }
+    }
+}
